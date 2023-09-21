bb1f524820962bde78459a108ed7a6be95add350
Vincent Bockaert <vincentbockaert@outlook.com>
2023-09-21 22:54:16 +0200

    bump sops version, awscli 1 to 2

--- containers/liquibase-sops-aws-rds/scripts/import-rds-root-ca.sh
+++ containers/liquibase-sops-aws-rds/scripts/import-rds-root-ca.sh
@@ -1,23 +1,30 @@
-#!/bin/sh
-# original authors & credit: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.SSL-certificate-rotation.html#UsingWithRDS.SSL-certificate-rotation-sample-script
-
-mydir=/var/jdk-keystores
+#!/bin/bash
+mydir=tmp/certs
 if [ ! -e "${mydir}" ]
 then
 mkdir -p "${mydir}"
 fi
 
-truststore="${JAVA_HOME}/lib/security/cacerts"
-storepassword=changeit # only use plaintext password for cicd/build containers not production worker containers
+truststore=${JAVA_HOME}/lib/security/cacerts
+storepassword=changeit
 
-curl -sS "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem" > ${mydir}/global-bundle.pem
-awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1}{print > "rds-ca-" n ".pem"}' < ${mydir}/global-bundle.pem
+curl -sS "https://truststore.pki.rds.amazonaws.com/eu-west-1/eu-west-1-bundle.pem" > "${mydir}/eu-west-1-bundle.pem"
+awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1}{print > "rds-ca-" n ".pem"}' < "${mydir}/eu-west-1-bundle.pem"
 
 for CERT in rds-ca-*; do
-  alias=$(openssl x509 -noout -text -in $CERT | perl -ne 'next unless /Subject:/; s/.*(CN=|CN = )//; print')
-  echo "Importing $alias"
-  keytool -import -file ${CERT} -cacerts -storepass ${storepassword} -alias "${alias}" -noprompt
-  rm $CERT
+  alias=$(openssl x509 -noout -text -in "${CERT}" | perl -ne 'next unless /Subject:/; s/.*(CN=|CN = )//; print')
+  echo "Importing ${alias}"
+  keytool -import -file "${CERT}" -alias "${alias}" -storepass "${storepassword}" -keystore "${truststore}" -noprompt
+  rm "${CERT}"
 done
 
-rm ${mydir}/global-bundle.pem        
+rm "${mydir}/eu-west-1-bundle.pem"
+
+echo "Trust store content is: "
+
+keytool -list -v -keystore "${truststore}" -storepass "${storepassword}" | grep Alias | cut -d " " -f3- | while read -r alias
+do
+   expiry=$(keytool -list -v -keystore "${truststore}" -storepass "${storepassword}" -alias "${alias}" | grep Valid | perl -ne 'if(/until: (.*?)\n/) { print "$1\n"; }')
+   echo " Certificate ${alias} expires in '$expiry'" 
+done
+                
